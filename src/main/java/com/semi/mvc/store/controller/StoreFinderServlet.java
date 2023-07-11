13ae74989a7feaef44f50c94ce18d20520b160f4
justinajin <justinajin@gmail.com>
2023-07-12 01:02:48 +0900

    Merge branch 'master' of https://github.com/MinHeeJ/semiProject

---
+++ src/main/java/com/semi/mvc/store/controller/StoreFinderServlet.java
@@ -0,0 +1,27 @@
+package com.semi.mvc.store.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StoreFinderServlet
+ */
+@WebServlet("/store/storeFinder")
+public class StoreFinderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1.입력값처리
+		String searchKeyword = request.getParameter("searchKeyword");
+		
+		
+	}
+
+}
