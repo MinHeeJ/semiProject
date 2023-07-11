2055afa4fdc518d9b1ee54eb5e830f513952cef4
MIROO1220 <mir0_0@icloud.com>
2023-07-11 17:53:45 +0900

    미루커밋

--- src/main/java/com/semi/mvc/common/JdbcTemplate.java
+++ src/main/java/com/semi/mvc/common/JdbcTemplate.java
@@ -1,6 +1,8 @@
 package com.semi.mvc.common;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.naming.Context;
@@ -9,6 +11,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class JdbcTemplate {
+	
 	public static Connection getConnection() {
 		Connection conn = null;
 		try {
@@ -19,5 +22,50 @@ public class JdbcTemplate {
 			e.printStackTrace();
 		}
 		return conn;
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed())
+				conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed())
+				conn.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed())
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public static void close(PreparedStatement pstmt) {
+		try {
+			if(pstmt != null && !pstmt.isClosed())
+				pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed())
+				rset.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
 	}
 }
