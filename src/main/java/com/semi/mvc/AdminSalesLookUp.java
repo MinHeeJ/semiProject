98d9f9df46487138e150c6af8b2477e885fc7f32
MIROO1220 <mir0_0@icloud.com>
2023-07-12 17:52:42 +0900

    매출조회.. 무섭다...

--- src/main/java/com/semi/mvc/AdminSalesLookUp.java
+++ src/main/java/com/semi/mvc/AdminSalesLookUp.java
@@ -1,7 +1,6 @@
-package com.semi.mvc.admin.controller;
+package com.semi.mvc;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.semi.mvc.member.model.service.MemberService;
-import com.semi.mvc.member.model.vo.Member;
 
 /**
- * Servlet implementation class AdminMemberListServlet
+ * Servlet implementation class AdminSalesLookUp
  */
-@WebServlet("/admin/memberList")
-public class AdminMemberListServlet extends HttpServlet {
+@WebServlet("/admin/salesLookUp")
+public class AdminSalesLookUp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final MemberService memberService = new MemberService();
 
@@ -24,15 +22,15 @@ public class AdminMemberListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 입력값 처리 x
+		// 1. 사용자 입력값 처리 x
 		
 		// 2. 업무로직
-		List<Member> members = memberService.findAll();
+//		int countAll = memberService.salesLookUp();
 		
-		request.setAttribute("members", members);
+//		request.setAttribute("countAll", countAll);
 		
 		// 3. 응답처리
-		request.getRequestDispatcher("/WEB-INF/views/admin/memberList.jsp")
+		request.getRequestDispatcher("/WEB/INF/views/admin/salesLookUp.jsp")
 			.forward(request, response);
 	}
 
