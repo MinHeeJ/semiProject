4c6efe666db2d12c2fcea5b175ab4dc6d5fe9fb5
justinajin <justinajin@gmail.com>
2023-07-12 16:52:50 +0900

    4시 52분 커밋

--- src/main/java/com/semi/mvc/store/model/service/StoreService.java
+++ src/main/java/com/semi/mvc/store/model/service/StoreService.java
@@ -4,6 +4,9 @@ import static com.semi.mvc.common.JdbcTemplate.*;
 import java.sql.Connection;
 import java.util.List;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
+import com.semi.mvc.store.model.dao.StoreDao;
 import com.semi.mvc.store.model.vo.Store;
 
 public class StoreService {
@@ -12,8 +15,23 @@ public class StoreService {
 	public List<Store> searchStore(String searchStore) {
 		Connection conn = getConnection();
 		List<Store> stores = storeDao.searchStore(conn, searchStore);
+		close(conn);
+		return stores;
 	}
 
-	
+	public List<Store> findAll(int start, int end) {
+		Connection conn = getConnection();
+		List<Store> stores = storeDao.findAll(conn, start, end);
+		close(conn);
+		
+		return stores;
+	}
 
+	public int getTotalContent() {
+		Connection conn = getConnection();
+		int totalContent = storeDao.getTotalContent(conn);
+		close(conn);
+		return totalContent;
+	}
+	
 }
